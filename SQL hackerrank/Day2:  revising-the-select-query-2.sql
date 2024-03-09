44b3bc7272c3c60a647803f44ad6ceb1ce7c7904
Aditya Gaikwad <131509238+aditya-1123@users.noreply.github.com>
2024-03-10 00:10:06 +0530

    Create Day2:  revising-the-select-query-2.sql

---
+++ SQL hackerrank/Day2:  revising-the-select-query-2.sql
@@ -0,0 +1,4 @@
+/* Problem Link https://www.hackerrank.com/challenges/revising-the-select-query-2/submissions/code/372543605 */
+
+select name from city
+where countrycode="USA" and population>120000;
