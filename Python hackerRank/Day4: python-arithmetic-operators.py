facded6fa421d2c352d75f1ac030d5d0ac28aea2
Aditya Gaikwad <131509238+aditya-1123@users.noreply.github.com>
2024-03-11 23:04:12 +0530

    Create Day4: python-arithmetic-operators.py

---
+++ Python hackerRank/Day4: python-arithmetic-operators.py
@@ -0,0 +1,7 @@
+# Problem link : https://www.hackerrank.com/challenges/python-arithmetic-operators/submissions/code/271325928
+
+(a, b) = (int(input()), int(input()))
+
+print(a+b)
+print(a-b)
+print(a*b)
