287126dd8e64e1e889f1b91f521a1717f12c121a
Aditya Gaikwad <131509238+aditya-1123@users.noreply.github.com>
2024-03-11 23:00:01 +0530

    Create Day3: whats-your-name.py

---
+++ Python hackerRank/Day3: whats-your-name.py
@@ -0,0 +1,5 @@
+# Problem link : https://www.hackerrank.com/challenges/whats-your-name/submissions/code/318762086
+
+def print_full_name(first, last):
+    # Write your code here
+    print("Hello "+first,last+"! You just delved into python.")
