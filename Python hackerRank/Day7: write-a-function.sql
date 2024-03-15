ad5a94b07b97d3db9e69527711d5f8813c322cee
Aditya Gaikwad <131509238+aditya-1123@users.noreply.github.com>
2024-03-15 23:06:43 +0530

    Create Day7: write-a-function.sql

---
+++ Python hackerRank/Day7: write-a-function.sql
@@ -0,0 +1,16 @@
+''' Problem Link : https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true    ''' 
+
+def is_leap(year):
+    
+    # Write your logic here
+    if(year%4==0):
+        if(year%100==0):
+            if(year%400==0):
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+        
